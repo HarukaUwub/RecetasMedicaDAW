fadb9628a01f1c0a6ffbd4813fb2b5ad7852aeee
HarukaUwub <wariom360@live.com.mx>
2025-12-04 05:17:37 -0600

    borrar
    .

--- app/dbtest.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import create_engine
-
-engine = create_engine("mysql+mysqlconnector://root:@localhost/recetario_db")
-try:
-    connection = engine.connect()
-    print("Conectado a MySQL correctamente")
-except Exception as e:
-    print("Error al conectar:", e)
--- app/listar_recetas.py
+++
@@ -1,6 +0,0 @@
-# listar_recetas.py
-from database import Session
-from models import Receta
-session = Session()
-for r in session.query(Receta).all():
-    print(r.id, r.diagnostico, r.fecha, r.paciente.nombre, r.medico.nombre)
